bbac17162a0263134b68023b6419d66f22a7537e
GennadyS <gennplus@gmail.com>
2023-11-26 00:47:54 +0100

    Add FileReader method to read file to list of strings and test.

--- src/main/java/org/example/samlpes/MyFileReader.java
+++ src/main/java/org/example/samlpes/MyFileReader.java
@@ -3,11 +3,13 @@ package org.example.samlpes;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.util.LinkedList;
+import java.util.List;
 
 public class MyFileReader {
 
     // N.B. for some reason CRLF is added to the end of file.
-    public static String readToString(File file){
+    public static String readToString(File file) {
         String content = "";
         try {
             // option 1
@@ -15,6 +17,16 @@ public class MyFileReader {
 
             // option 2 (preferred by IntelliJ)
             content = Files.readString(file.toPath());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return content;
+    }
+
+    public static List<String> readToListOfStrings(File file) {
+        List<String> content = new LinkedList<>();
+        try {
+            content = Files.readAllLines(file.toPath());
         } catch (IOException e) {
             e.printStackTrace();
         }
