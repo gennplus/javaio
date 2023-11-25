fd330b5d4f2f6791ec4b2f138ab10b2fe3fd3774
GennadyS <gennplus@gmail.com>
2023-11-25 23:41:05 +0100

    Add Downloader, samples and it's tests.

--- src/main/java/org/example/samlpes/Downloader.java
+++ src/main/java/org/example/samlpes/Downloader.java
@@ -1,9 +1,53 @@
 package org.example.samlpes;
 
-import java.io.InputStream;
+import java.io.*;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Downloader {
-    public static InputStream downloadFile(String url) {
-        return null;
+    public static String readFileAsString(String urlAsString) throws IOException {
+        URL url = new URL(urlAsString);
+        InputStream in = url.openStream();
+        InputStreamReader isr = new InputStreamReader(in);
+        BufferedReader br = new BufferedReader(isr);
+
+        StringBuilder content = new StringBuilder();
+        String line;
+        while ((line = br.readLine()) != null) {
+            content.append(line).append("\n");
+        }
+
+        br.close();
+        return content.toString();
+    }
+
+    public static List<String> readFile(String urlAsString) throws IOException {
+        URL url = new URL(urlAsString);
+        InputStream in = url.openStream();
+        InputStreamReader isr = new InputStreamReader(in);
+        BufferedReader br = new BufferedReader(isr);
+
+        List<String> content = new ArrayList<>();
+        String line;
+        while ((line = br.readLine()) != null) {
+            content.add(line);
+        }
+
+        return content;
+    }
+
+    public static void downloadFileAndSave(String urlAsString, String newFileName) throws IOException {
+
+        // Download the file from the URL to the file
+        URL url = new URL(urlAsString);
+        try (InputStream in = url.openStream();
+            OutputStream out = new FileOutputStream(newFileName)) {
+            byte[] buffer = new byte[1024];
+            int bytesRead;
+            while ((bytesRead = in.read(buffer)) != -1) {
+                out.write(buffer, 0, bytesRead);
+            }
+        }
     }
 }
---
+++ src/test/java/org/example/samlpes/DownloaderTest.java
@@ -0,0 +1,65 @@
+package org.example.samlpes;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+
+class DownloaderTest {
+
+    private static final String TEST_FILE1_URL = "https://github.com/gennplus/javaio/blob/master/test1.txt";
+    private static final String TEST_FILE2_URL = "https://github.com/gennplus/javaio/blob/master/test2.txt";
+
+    @Test
+    void readFileAsString_caseCrLfEnded() {
+        try {
+            String actual = Downloader.readFileAsString(TEST_FILE1_URL);
+            String expected = "test1\ntest2\n";
+            Assertions.assertEquals(expected, actual);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    void readFileAsString_caseVisibleSymbolEnded() {
+        try {
+            String actual = Downloader.readFileAsString(TEST_FILE2_URL);
+            String expected = "test1\ntest2";
+            Assertions.assertEquals(expected, actual);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    void readFile_caseCrLfEnded() {
+        try {
+            List<String> actual = Downloader.readFile(TEST_FILE1_URL);
+            List<String> expected = Arrays.asList("test1", "test2");
+            Assertions.assertEquals(actual, expected);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    @Test
+    void downloadFileAndSave_caseCrLfEnded() {
+        try {
+            Downloader.downloadFileAndSave(TEST_FILE1_URL, "mytest.txt");
+            String actual = new String(Files.readAllBytes(Paths.get("mytest.txt")));
+            String expected = "test1\ntest2\n";
+
+            Assertions.assertEquals(expected, actual);
+            Assertions.assertTrue(new File("mytest.txt").isFile());
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
